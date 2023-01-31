f403bde135674486ee9f8e86f563905ce2ac914d
Bruno Bincoletto <bruno.bincoletto@mercadolivre.com.br>
2023-01-31 18:29:56 -0300

    change clean install

--- commands/clean-install.sh
+++ commands/clean-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [ -f "package-lock.json" ]; then
-  rm -rf ./node_modules && rm package-lock.json && npm cache clean --force && nvm use && npm i
+  rm -rf ./node_modules;
+  rm package-lock.json;
+  npm cache clean --force;
+  nvm use;
+  npm i;
   echo "Clean install executed successfully!"
 else
   echo "We couldn't find the package-lock.json file, try running the command manually. \n\n rm -rf ./node_modules && rm package-lock.json && npm cache clean --force && nvm use && npm i"
