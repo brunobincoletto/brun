efa817eeb290afe86b5ea21d950cb89058141f5a
Bruno Bincoletto <bruno.bincoletto@mercadolivre.com.br>
2023-02-01 13:44:32 -0300

    simplify branch rename

--- commands/branch-rename.sh
+++ commands/branch-rename.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
+curbranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
 if [ -d ".git" ]; then
-  read -p "Enter the name of the branch you want to rename: " curbranch
-  if [ -z $curbranch ]; then
-    clear
-    echo -e "Run the script again and enter the current name of the branch you want to rename..."
-    exit
-  fi
-  echo "Selecting the $curbranch you want to rename..."
-  git checkout $curbranch
   read -p "Enter the NEW branch name: " newname
   if [ -z $newname ]; then
-    clear
-    echo -e "Run the script again and enter a new valid name that you want to rename..."
-    exit
+    clear;
+    echo -e "Run the script again and enter a new valid name that you want to rename...";
+    exit;
   fi
   echo "Creating a new branch with the new name: $newname"
   git branch -m $newname
@@ -21,6 +14,6 @@ if [ -d ".git" ]; then
   git push origin --delete $curbranch
   echo "DONE! Now your branch $curbranch is called $newname !!!"
 else
-  echo "We didn't find GIT in this folder, start again :("
+  echo "We didn't find GIT in this project, start again :("
   exit
 fi
